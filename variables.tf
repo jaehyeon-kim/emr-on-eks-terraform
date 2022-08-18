4ad8c4b3950e5f764b1c703e03024b838b6b17d5
Jaehyeon Kim <jaehyeon.kim@energyaustralia.com.au>
2022-08-18 18:54:04 +1000

    update variables

--- variables.tf
+++ variables.tf
@@ -11,21 +11,29 @@ variable "admin_password" {
 }
 
 locals {
-  name        = basename(path.cwd)
-  region      = data.aws_region.current.name
-  environment = "dev"
+  name             = basename(path.cwd)
+  region           = data.aws_region.current.name
+  environment      = "dev"
+  local_ip_address = "${chomp(data.http.local_ip_address.response_body)}/32"
+
+  flag = {
+    vpn = {
+      to_create        = true
+      to_limit_ingress = true
+      to_use_spot      = false
+    }
+    data_bucket = {
+      to_create = false
+    }
+  }
 
   vpc = {
     class_b = "100" # Class B of VPC (10.XXX.0.0/16)
   }
 
   vpn = {
-    to_create        = true
-    limit_ingress    = true
-    use_spot         = false
-    admin_username   = "master"
-    local_id_address = "${chomp(data.http.local_ip_address.body)}/32"
-    ingress_cidr     = local.vpn.limit_ingress ? local.vpn.local_ip_address : "0.0.0.0/0"
+    admin_username = "master"
+    ingress_cidr   = local.flag.vpn.to_limit_ingress ? local.local_ip_address : "0.0.0.0/0"
     spot_override = [
       { instance_type : "t3.nano" },
       { instance_type : "t3a.nano" },
@@ -33,7 +41,6 @@ locals {
   }
 
   data_bucket = {
-    to_create   = true
     bucket_name = "${local.name}-data-${data.aws_caller_identity.current.account_id}-${local.region}"
   }
 
